c615505f7da83e7b1cbaa3b738bb2c32c962b37c
Sergey Crew <sergeycrew.bs@gmail.com>
2022-07-08 01:00:33 -0700

    homework created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from 'react';
+import { Section } from 'Components/Section';
+import { FeedbackOptions } from './Components/FeedbackOptions';
+import { Statistics } from './Components/Statistics/Statistics';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  OnLeaveFeedback = ({ target: { name } }) => {
+    this.setState(prevState => ({
+      ...prevState,
+      [name]: prevState[name] + 1,
+    }));
+  };
+
+  countTotalFeedback = () => {
+    let total = 0;
+    for (let value of Object.values(this.state)) {
+      total += value;
+    }
+    return total;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const total = this.countTotalFeedback();
+    return total ? Math.round((this.state.good / total) * 100) : 0;
+  };
+
+  render() {
+    const options = Object.keys(this.state);
+    const { good, neutral, bad } = this.state;
+    return (
+      <>
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={options}
+            OnLeaveFeedback={this.OnLeaveFeedback}
+          />
+        </Section>
+        <Statistics
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          total={this.countTotalFeedback()}
+          positivePercentage={this.countPositiveFeedbackPercentage()}
+        />
+      </>
+    );
+  }
 }
-
-export default App;
